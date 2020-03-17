6ed9c7024e55caa9be510acfe3254596d28c49c2
Kurt Opel <kurt.opel@plantemoran.com>
2020-03-17 16:32:12 -0400

    working app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng9-firebase-realtime-db-crud';
+  title = 'Angular 9 Firebase Realtime Database CRUD';
+  description = 'Angular-Fire-Demo';
+
+  nameValue = '';
+  descriptionValue = '';
+  todos: Observable<Todo[]>;
+
+  constructor(public db:AngularFireDatabase){
+    this.todos = db.list<Todo>('todos').valueChanges();
+  }
+
+  onSubmit(){
+    this.db.list('todos').push({name:this.nameValue,description:this.descriptionValue});
+    this.nameValue = '';
+    this.descriptionValue = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// angular imports
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// firebase imports
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { environment } from "../environments/environment";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
